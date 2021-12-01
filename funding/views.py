259e625e94e9e4978dedfcd22fddc25717ff084a
hanimohsen3810 <hanimohsen3810@yahoo.com.com>
2021-12-01 18:25:51 +0200

    reg remove

--- funding/views.py
+++ funding/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from .models import Funding
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 
 # Create your views here.
 def funding_list(request):
     funding_list = Funding.objects.all()
-    context = {'funding_list': funding_list}
+
+    paginator = Paginator(funding_list, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'funding_list': funding_list, 'listlength': funding_list}
     return render(request, 'funding/funding_list.html', context)
 
 
